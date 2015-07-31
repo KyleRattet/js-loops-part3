357aa52369932a195fcc94b0de3f8af9e6332077
KyleRattet <krattet@gmail.com>
2015-07-31 15:27:22 -0600

    solution to problem 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,62 @@
 // add scripts
 
 $(document).on('ready', function() {
-  console.log('sanity check!');
+
+//GLOBAL VARIABLES
+  var students = [
+  {
+   name: 'Derek',
+   age: 25,
+   city: 'Boulder'
+  },
+  {
+   name: 'Liz',
+   age: 28,
+   city: 'Denver'
+  },
+  {
+   name: 'Carl',
+   age: 31,
+   city: 'Boulder'
+  },
+  {
+   name: 'Peter',
+   age: 23,
+   city: 'Boulder'
+  },
+  {
+   name: 'Megan',
+   age: 19,
+   city: 'Denver'
+  }
+];
+
+//EXAMPLE (OUTPUT DEREK LIZ CARL PETER MEGAN)
+
+// function firstLoop (arr) {
+//   arr.forEach(function (person) {
+//     console.log(person.name);
+//   });
+// }
+
+// firstLoop(students);
+
+//PROBLEM 1 (PRINT OUT AGES)
+function nameLoop (arr) {
+  arr.forEach(function (person) {
+    console.log(person.age);
+  });
+}
+
+nameLoop(students);
+
 });
+
+
+
+
+
+
+
+
+
